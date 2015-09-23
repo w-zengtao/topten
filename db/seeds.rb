5626efa323e319976d6704ba33559c66877c7c69
ZengWeimao <so.zengtao@gmail.com>
2015-09-23 17:37:29 +0800

    update readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+
 Nation.all.each do |nation|
   print nation.id
   puts '-' * 80
